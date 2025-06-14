25dd3e3d82c312a42adc02c3c03d389e5d138f25
wendres <wendres.araujo@selpe.com.br>
2025-06-14 14:34:40 -0300

    criando migration de logs da api

---
+++ backend/database/migrations/2025_06_14_173338_create_api_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('api_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('method');
+            $table->string('endpoint');
+            $table->json('payload')->nullable();
+            $table->integer('status_code');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('api_logs');
+    }
+};
