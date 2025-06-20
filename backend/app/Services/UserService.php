abee6871649aaeb96774438180ee3332d075e635
wendres <wendres.araujo@selpe.com.br>
2025-06-20 15:48:44 -0300

    cacheando o json do response

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -9,48 +9,61 @@ use Illuminate\Support\Facades\Cache;
 class UserService
 {
     protected $baseUrl = 'https://api.github.com/users/';
+    protected $tempStatusCode;
 
     public function getUser(string $username): ?array
     {
         $endpoint = $this->baseUrl . $username;
 
-        $response = Cache::remember("github_user_{$username}", now()->addMinutes(30), function () use ($endpoint) {
+        $userDataResponse = Cache::remember("github_user_{$username}", now()->addMinutes(30), function () use ($endpoint) {
             $response = Http::get($endpoint);
 
             if ($response->failed()) {
                 throw new \Exception('Erro ao obter usuario.', $response->status());
             }
+
+            $this->tempStatusCode = $response->status();
+
+            return $response->json();
         });
+
+        $statusCode = $this->tempStatusCode ?? 200;
 
         $this->logRequest([
             'method' => 'GET',
             'endpoint' => $endpoint,
             'payload' => null,
-            'status_code' => $response->status(),
+            'status_code' => $statusCode,
         ]);
 
-        return $response->json();
+        return $userDataResponse;
     }
 
     public function getFollowings(string $username): ?array
     {
         $endpoint = $this->baseUrl . $username . '/following';
-        $response = Cache::remember("github_followings_{$username}", now()->addMinutes(30), function () use ($endpoint) {
+
+        $followingsData = Cache::remember("github_followings_{$username}", now()->addMinutes(30), function () use ($endpoint) {
             $response = Http::get($endpoint);
 
             if ($response->failed()) {
                 throw new \Exception('Erro ao obter followings.', $response->status());
             }
+
+            $this->tempStatusCode = $response->status();
+
+            return $response->json();
         });
 
+        $statusCode = $this->tempStatusCode ?? 200;
         $this->logRequest([
             'method' => 'GET',
             'endpoint' => $endpoint,
             'payload' => null,
-            'status_code' => $response->status(),
+            'status_code' => $statusCode,
         ]);
 
-        return $response->json();
+        return $followingsData;
     }
 
     protected function logRequest(array $data)
